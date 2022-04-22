330cdae74fbf3a2ece7231307d501ca0dfbd0cea
rehemae <rehemaephraimnanda@gmail.com>
2022-04-22 10:31:10 +0300

    exercises python

---
+++ project.py
@@ -0,0 +1,29 @@
+successful=True
+for number in range(3):
+    print("attempt",number)
+    if successful:
+        print("successful")
+        break
+    else:
+        print("Attempt 3  times failed")
+    
+        for x in range(5):
+            for y in range(3):
+                print(f"({x}),{y}")
+                def multiply(*numbers):
+                  total=1
+                  for number in numbers:
+                      total*=number
+                      return total
+                  
+                  
+                  
+                  print ("start")
+                  print(multiply(1,2,3,))
+                  multiply
+                  
+                    
+            
+          
+            
+          
